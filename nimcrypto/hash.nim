80eb5a99b91b3f83478db9f60c4cb67b2a939b3a
cheatfate <ka@hardcore.kiev.ua>
2018-03-20 12:54:31 +0200

    Fix bounderies check.

--- nimcrypto/hash.nim
+++ nimcrypto/hash.nim
@@ -31,6 +31,7 @@ proc digest*[T](HashType: typedesc, data: openarray[T],
   let eo = if ofinish == -1: uint(len(data)) else: uint(ofinish - so)
   ctx.init()
   assert(uint(so) <= eo)
+  assert(eo <= len(data))
   if eo == 0:
     result = ctx.finish()
   else:

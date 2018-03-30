d6e19755541d5de7f83e54647fc59628fd4a17f8
cheatfate <ka@hardcore.kiev.ua>
2018-03-30 10:53:19 +0300

    Fix compiler crash workaround.

--- nimcrypto/hmac.nim
+++ nimcrypto/hmac.nim
@@ -46,9 +46,9 @@ template sizeBlock*(h: HMAC[KeccakContext]): uint =
   else:
     {.fatal: "Choosen hash primitive is not yet supported!".}
 
-template sizeDigest*(h: HMAC[Sha2Context]): uint = Sha2Context.bits
-template sizeDigest*(h: HMAC[RipemdContext]): uint = RipemdContext.bits
-template sizeDigest*(h: HMAC[KeccakContext]): uint = KeccakContext.bits
+template sizeDigest*(h: HMAC[Sha2Context]): uint = uint(h.bits)
+template sizeDigest*(h: HMAC[RipemdContext]): uint = uint(h.bits)
+template sizeDigest*(h: HMAC[KeccakContext]): uint = uint(h.bits)
 
 proc init*[T](hmctx: var HMAC[T], key: ptr byte, ulen: uint) =
   mixin init, update, finish

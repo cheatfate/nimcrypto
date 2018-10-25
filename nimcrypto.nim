74ec0c9a382bcf4e0bc6ef80c021a4a06ecad7f2
Bruno Skvorc <bruno@skvorc.me>
2018-10-25 16:20:14 +0200

    Added some things and docs skeleton

--- nimcrypto.nim
+++ nimcrypto.nim
@@ -11,5 +11,9 @@ import nimcrypto/[hash, sha2, ripemd, keccak, blake2, hmac]
 import nimcrypto/[rijndael, blowfish, twofish, bcmode]
 import nimcrypto/[utils, sysrand]
 
+## Nimcrypto is the Nim language's cryptographic library. 
+## It implements several popular cryptographic algorithms and their tests, 
+## with some examples in the official repo: https://github.com/cheatfate/nimcrypto/tree/master/examples
+
 export hash, sha2, ripemd, keccak, blake2, hmac, rijndael, twofish, blowfish,
        bcmode, utils, sysrand

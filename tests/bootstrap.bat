a5742a9a214ac33f91615f3862c7b099aec43b00
Eugene Kabanov <ka@hardcore.kiev.ua>
2021-06-19 02:28:08 +0300

    Use Github Actions instead of Travis & Appveyor. (#52)
    * Add Github Actions CI instead of Travis and Appveyor.

--- tests/bootstrap.bat
+++ tests/bootstrap.bat
@@ -1,25 +1,61 @@
-@echo off
-IF NOT EXIST "%CD%\%NIM_DIR%\bin\nim.exe" (
-  echo Building Nim [%NIM_BRANCH%] in %NIM_DIR%
-  git clone https://github.com/nim-lang/Nim.git "%CD%\%NIM_DIR%"
-  cd "%CD%\%NIM_DIR%"
-  IF NOT "%NIM_BRANCH%" == "devel" (
-    git checkout "tags/%NIM_BRANCH%" -b "%NIM_BRANCH%"
-  ) ELSE (
-    git checkout devel
-  )
-  git clone --depth 1 https://github.com/nim-lang/csources
-  cd csources
-  IF "%PLATFORM%" == "x64" ( build64.bat ) else ( build.bat )
-  cd ..
-  bin\nim c -d:release koch
-  koch boot -d:release
-  koch nimble
-  cd ..
-) ELSE (
-  cd "%CD%\%NIM_DIR%"
-  set GITBRANCH=
-  for /f %%I in ('git.exe rev-parse --abbrev-ref HEAD 2^> NUL') do set GITBRANCH=%%I
-  echo Using Nim [%GITBRANCH%] in %NIM_DIR%
-  cd ..
+@ECHO OFF
+
+SETLOCAL enabledelayedexpansion
+
+IF "%NIM_DIR%" == "" (
+  ECHO NIM_DIR variable is not set
+  EXIT /B 0
 )
+
+IF "%NIM_BRANCH%" == "" (
+  ECHO NIM_BRANCH variable is not set
+  EXIT /B 0
+)
+
+IF "%NIM_ARCH%" == "" (
+  ECHO NIM_ARCH variable is not set
+  EXIT /B 0
+)
+
+IF NOT EXIST "%CD%\%NIM_DIR%\bin\nim.exe" (
+  CALL :BUILD_NIM
+) ELSE (
+  CALL :USE_NIM
+)
+EXIT /B 0
+
+:BUILD_NIM
+ECHO Building Nim [%NIM_BRANCH%] (%NIM_ARCH%) in %NIM_DIR%
+git clone https://github.com/nim-lang/Nim.git "%CD%\%NIM_DIR%"
+CD "%CD%\%NIM_DIR%"
+IF NOT "%NIM_BRANCH%" == "devel" (
+  git checkout "tags/%NIM_BRANCH%" -b "%NIM_BRANCH%"
+) ELSE (
+  git checkout devel
+)
+git clone --depth 1 https://github.com/nim-lang/csources_v1
+CD csources_v1
+
+IF "%NIM_ARCH%" == "amd64" (CALL build64.bat) ELSE (CALL build32.bat)
+CD ..
+ECHO CSOURCES NIM DONE
+bin\nim c --skipParentCfg -d:release koch
+koch boot -d:release --skipParentCfg
+koch nimble --skipParentCfg
+CD ..
+EXIT /B 0
+
+:USE_NIM
+CD "%CD%\%NIM_DIR%"
+FOR /F "tokens=3" %%I IN ('git status ^| head -1') DO SET GITBRANCH=%%I
+ECHO Found Nim [%GITBRANCH%] in %NIM_DIR%
+IF "%GITBRANCH%" == "%NIM_BRANCH%" (
+  ECHO Using Nim [%GITBRANCH%] in %NIM_DIR%
+  CD ..
+) ELSE (
+  CD ..
+  RMDIR /S /Q "%NIM_DIR%"
+  CALL :BUILD_NIM
+)
+
+EXIT /B 0
--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -1,21 +1,49 @@
 #!/usr/bin/env bash
-if [ ! -e "$NIM_DIR/bin/nim" ]; then
-  echo "Building Nim [$NIM_BRANCH] in $NIM_DIR"
-  git clone https://github.com/nim-lang/Nim.git "$NIM_DIR"
-  cd "$NIM_DIR"
-  if [ "$NIM_BRANCH" = "devel" ]; then
+
+if [[ -z "${NIM_DIR}" ]]; then
+  echo "NIM_DIR variable is not set"
+  exit 1
+fi
+
+if [[ -z "${NIM_BRANCH}" ]]; then
+  echo "NIM_BRANCH variable is not set"
+  exit 1
+fi
+
+function build_nim {
+  echo "Building Nim [${NIM_BRANCH}] in ${NIM_DIR}"
+  git clone https://github.com/nim-lang/Nim.git ${NIM_DIR}
+  cd "${NIM_DIR}"
+  if [ "${NIM_BRANCH}" = "devel" ]; then
     git checkout devel
   else
-    git checkout "tags/$NIM_BRANCH" -b "$NIM_BRANCH"
+    git checkout "tags/${NIM_BRANCH}" -b "${NIM_BRANCH}"
   fi
-  git clone --depth 1 https://github.com/nim-lang/csources
-  cd csources && sh build.sh
+  git clone --depth 1 https://github.com/nim-lang/csources_v1
+  cd csources_v1 && sh build.sh
   cd ..
   bin/nim c -d:release koch
   ./koch boot -d:release
   ./koch nimble -d:release
   cd ..
-else
+}
+
+function use_nim {
+  cd "${NIM_DIR}"
   GITBRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-  echo "Using Nim [$GITBRANCH] in $NIM_DIR"
+  echo "Found Nim [${GITBRANCH}] in ${NIM_DIR}"
+  if [ "${GITBRANCH}" = "${NIM_BRANCH}" ]; then
+    echo "Using Nim [${GITBRANCH}] in ${NIM_DIR}"
+    cd ..
+  else
+    cd ..
+    rm -rf "${NIM_DIR}"
+    build_nim
+  fi
+}
+
+if [ ! -e "${NIM_DIR}/bin/nim" ]; then
+  build_nim
+else
+  use_nim
 fi

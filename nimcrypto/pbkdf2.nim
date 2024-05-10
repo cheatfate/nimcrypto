ae7a3ab512f4200ca32c39092f9a290f28ea1545
cheatfate <eugene.kabanov@status.im>
2024-05-10 05:49:20 +0300

    Add optimized PBKDF2[HMAC[sha2]] primitives and tests.

--- nimcrypto/pbkdf2.nim
+++ nimcrypto/pbkdf2.nim
@@ -1,7 +1,7 @@
 #
 #
 #                    NimCrypto
-#        (c) Copyright 2018 Eugene Kabanov
+#      (c) Copyright 2018-2024 Eugene Kabanov
 #
 #      See the file "LICENSE", included in this
 #    distribution, for details about the copyright.
@@ -12,8 +12,11 @@
 ##
 ## Tests for PBKDF2-HMAC-SHA224/256/384/512 made according to
 ## [https://github.com/Anti-weakpasswords/PBKDF2-Test-Vectors/releases]
-import hmac, utils
-export hmac
+
+{.push raises: [].}
+
+import "."/[hmac, utils, cpufeatures]
+export hmac, cpufeatures
 
 proc pbkdf2*[T, M, N](ctx: var HMAC[T], password: openArray[M],
                       salt: openArray[N], c: int,
@@ -111,3 +114,103 @@ proc pbkdf2*[T, M](hashtype: typedesc, password: openArray[T],
     res.setLen(outlen)
     discard pbkdf2(ctx, password, salt, c, res)
   res
+
+## Optimized SHA2 declarations
+
+type
+  Sha2Type = sha224 | sha256 | sha384 | sha512 | sha512_224 | sha512_256
+
+proc pbkdf2*[M, N](ctx: var HMAC[Sha2Type], password: openArray[M],
+                   salt: openArray[N], c: int,
+                   output: var openArray[byte],
+                   implementation: Sha2Implementation,
+                   cpuFeatures: set[CpuFeature] = {}): int =
+  ## Calculate PBKDF2 result using HMAC algorithm `ctx`.
+  ##
+  ## ``ctx``      - HMAC[T] context
+  ## ``password`` - password string
+  ## ``salt``     - salt string
+  ## ``c``        - number of iterations
+  ## ``output``   - array of bytes where result will be stored.
+  ##
+  ## Returns number of bytes stored on success, or 0 on error.
+  mixin init, update, finish
+  var
+    counter: array[4, byte]
+    work: array[int(ctx.sizeDigest), byte]
+    md: array[int(ctx.sizeDigest), byte]
+    ctr: uint32
+    glength: int
+    bytesWrite: int
+
+  when not((M is byte) or (M is char)):
+    {.fatal: "Choosen password type is not supported!".}
+
+  when not((N is byte) or (N is char)):
+    {.fatal: "Choosen salt type is not supported!".}
+
+  when (sizeof(int) != 8) and (sizeof(int) != 4):
+    {.fatal: "Choosen architecture is not supported!".}
+
+  ctr = 1
+  glength = 0
+  let olength =
+    when sizeof(int) == 8:
+      min(len(output), int(0xFFFF_FFFF)) # 2^32 - 1
+    else:
+      len(output)
+
+  while glength < olength:
+    beStore32(counter, 0, ctr)
+    ctx.init(password, implementation, cpuFeatures)
+    ctx.update(salt)
+    ctx.update(counter)
+    discard ctx.finish(md)
+    work = md
+    for i in 1 ..< c:
+      ctx.init(password, implementation, cpuFeatures)
+      ctx.update(md)
+      discard ctx.finish(md)
+      for k in 0..<len(work):
+        work[k] = work[k] xor md[k]
+
+    bytesWrite = min(olength - glength, int(ctx.sizeDigest))
+    copyMem(output, glength, work, 0, bytesWrite)
+    glength = glength + bytesWrite
+    ctr = ctr + 1
+  ctx.clear()
+  int(glength)
+
+proc pbkdf2*[M, N](ctx: var HMAC[Sha2Type], password: openArray[M],
+                   salt: openArray[N], c: int,
+                   output: var openArray[byte],
+                   cpuFeatures: set[CpuFeature]): int =
+  pbkdf2(ctx, password, salt, c, output, Sha2Implementation.Auto, cpuFeatures)
+
+proc pbkdf2*[T, M](hashtype: typedesc[Sha2Type], password: openArray[T],
+                   salt: openArray[M], c: int,
+                   outlen: int,
+                   implementation: Sha2Implementation,
+                   cpuFeatures: set[CpuFeature] = {}): seq[byte] {.inline.} =
+  ## Calculate PBKDF2 result using HMAC[``hashtype``] algorithm.
+  ##
+  ## ``hashtype`` - hash algorithm which will be used in HMAC mode
+  ## ``password`` - password string
+  ## ``salt``     - salt string
+  ## ``c``        - number of iterations
+  ## ``outlen``   - length of bytes to be stored.
+  ##
+  ## Returns sequence of bytes.
+  var res: seq[byte]
+  if outlen > 0:
+    var ctx: HMAC[hashtype]
+    res.setLen(outlen)
+    discard pbkdf2(ctx, password, salt, c, res, implementation, cpuFeatures)
+  res
+
+proc pbkdf2*[T, M](hashtype: typedesc[Sha2Type], password: openArray[T],
+                   salt: openArray[M], c: int,
+                   outlen: int,
+                   cpuFeatures: set[CpuFeature]): seq[byte] {.inline.} =
+  pbkdf2(hashtype, password, salt, c, outlen, Sha2Implementation.Auto,
+         cpuFeatures)

79c8826b57eebcc53efd255faf0fe6eef8b1e53c
Eugene Kabanov <eugene.kabanov@status.im>
2025-05-07 14:45:03 +0300

    Fix scrypt generic sandwich via workaround. (#86)
    * Fix scrypt generic sandwich via workaround.
    Add test for scrypt usage via import.
    
    * Update CI script.
    
    * Add `version-2-2` to test matrix.

--- tests/testall.nim
+++ tests/testall.nim
@@ -3,4 +3,4 @@ import testsha1, testsha2, testkeccak, testripemd, testblake2
 import testhmac, testkdf, testscrypt
 import testrijndael, testblowfish, testtwofish
 import testbcmode
-import testapi, testutils
+import testapi, testutils, testissues

329da00494e7f4ee46ab1820b971f4bb94049be8
cheatfate <eugene.kabanov@status.im>
2025-12-24 04:00:41 +0200

    Fix bootstrap.sh.

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -14,19 +14,8 @@ function build_nim {
   echo "Building Nim [${NIM_BRANCH}] in ${NIM_DIR}"
   git clone https://github.com/nim-lang/Nim.git ${NIM_DIR}
   cd "${NIM_DIR}"
-  if [ "${NIM_BRANCH}" = "version-1-6" ]; then
-    git checkout "${NIM_BRANCH}"
-    git clone --depth 1 https://github.com/nim-lang/csources_v1
-    cd csources_v1 && sh build.sh
-  else
-    git checkout devel
-    git clone --depth 1 https://github.com/nim-lang/csources_v2
-    cd csources_v2 && sh build.sh
-  fi
-  cd ..
-  bin/nim c --skipParentCfg --noNimblePath --skipUserCfg -d:release koch
-  ./koch boot -d:release
-  ./koch nimble -d:release
+  git checkout "${NIM_BRANCH}"
+  ./build_all.sh
   cd ..
 }
 
